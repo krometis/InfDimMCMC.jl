ef8d50130ff627d892dd89c1dfb975e836dc3646
Justin Krometis <jkrometis@vt.edu>
2022-03-24 13:09:13 -0400

    mcmcAdapt: add mp-pcn

--- src/mcmcAdapt.jl
+++ src/mcmcAdapt.jl
@@ -11,6 +11,7 @@ function mcmcAdapt(mcmcP::mcmcProb, acceptRatio::Float64, targetAR::Float64; ver
     d["beta"] = min(1.0,d["beta"]); #beta>1 doesn't make sense
     mcmcSetSampler(mcmcP,d; verbose=0);
     (verbose > 0) && @printf("mcmcAdapt: acceptRatio=%5.3f, target=%5.3f. Adjusting step size beta by factor of %5.3f to %8.6f\n",acceptRatio, targetAR, adaptRatio, d["beta"]);
+
   elseif d["mcmc"] == :hmc
     d["eps"] *= adaptRatio;
     d["tau"] *= adaptRatio;
@@ -21,6 +22,12 @@ function mcmcAdapt(mcmcP::mcmcProb, acceptRatio::Float64, targetAR::Float64; ver
     d["h"] *= adaptRatio;
     mcmcSetSampler(mcmcP,d; verbose=0);
     (verbose > 0) && @printf("mcmcAdapt: acceptRatio=%5.3f, target=%5.3f. Adjusting step size h by factor of %5.3f to %8.6f\n",acceptRatio, targetAR, adaptRatio, d["h"]);
+
+  elseif d["mcmc"] == :mppcn
+    d["beta"] *= adaptRatio;
+    d["beta"] = min(1.0,d["beta"]); #beta>1 doesn't make sense
+    mcmcSetSampler(mcmcP,d; verbose=0);
+    (verbose > 0) && @printf("mcmcAdapt: acceptRatio=%5.3f, target=%5.3f. Adjusting step size beta by factor of %5.3f to %8.6f\n",acceptRatio, targetAR, adaptRatio, d["beta"]);
 
   else
     error("Adaptive MCMC not supported for selected method.");
