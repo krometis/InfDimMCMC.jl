60057eb64d68dcacb90495c0888dcd6461e71bd9
Justin Krometis <jkrometis@vt.edu>
2024-04-29 16:27:14 -0400

    add sspcn to a few other files

--- src/mcmcAdapt.jl
+++ src/mcmcAdapt.jl
@@ -23,7 +23,7 @@ function mcmcAdapt(mcmcP::mcmcProb, acceptRatio::Float64, targetAR::Float64; ver
     mcmcSetSampler(mcmcP,d; verbose=0);
     (verbose > 0) && @printf("mcmcAdapt: acceptRatio=%5.3f, target=%5.3f. Adjusting step size h by factor of %5.3f to %8.6f\n",acceptRatio, targetAR, adaptRatio, d["h"]);
 
-  elseif d["mcmc"] == :mppcn || d["mcmc"] == :bbpcn
+  elseif d["mcmc"] == :mppcn || d["mcmc"] == :bbpcn || d["mcmc"] == :sspcn
     d["beta"] *= adaptRatio;
     d["beta"] = min(1.0,d["beta"]); #beta>1 doesn't make sense
     mcmcSetSampler(mcmcP,d; verbose=0);
--- src/mcmcSetSampler.jl
+++ src/mcmcSetSampler.jl
@@ -35,6 +35,12 @@ function mcmcSetSampler(mcmcP::mcmcProb,d::Dict;verbose=1)
     mcmcP.mcmc = d;
     mcmcP.computeGradients = false;
 
+  elseif d["mcmc"] == :sspcn
+    (verbose > 0) && println("Setting sampler to Slingshot pCN (step size=$(d["beta"]), proposals=$(d["nprop"]))");
+    mcmcP.step = ( (cur,m;verbose=0) -> stepSsPcn(cur, m, d["beta"], d["nprop"]; verbose=verbose) );
+    mcmcP.mcmc = d;
+    mcmcP.computeGradients = false;
+
   elseif d["mcmc"] == :is
     (verbose > 0) && println("Setting sampler to IS");
     mcmcP.step = stepIS;
